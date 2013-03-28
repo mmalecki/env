b6bd7cf1faa0cf385b2e7919716a1d36783f4135
Maciej Małecki <me@mmalecki.com>
2013-03-28 03:56:36 +0100

    [api] Implement `env_set`

--- src/env.c
+++ src/env.c
@@ -3,33 +3,74 @@
 
 #include "env.h"
 
-char* env__find(char** env, char* name) {
+int env__find(char** env, char* name) {
   int i = 0;
   char* pair;
   unsigned int name_length = strlen(name);
 
   while ((pair = env[i++]) != NULL) {
     if (strncmp(name, pair, name_length) == 0) {
-      return pair;
+      return i - 1;
     }
   }
 
-  return NULL;
+  return -1;
+}
+
+unsigned int env_length(char** env) {
+  unsigned int i = 0;
+  while (env[i++] != NULL);
+  return i - 1;
 }
 
 char* env_get(char** env, char* name) {
+  int index;
   char* pair;
   char* result;
   unsigned int name_length, value_length;
 
   name_length = strlen(name);
-  pair = env__find(env, name);
+  index = env__find(env, name);
 
-  if (pair == NULL)
+  if ((index = env__find(env, name)) == -1)
     return NULL;
+
+  pair = env[index];
 
   value_length = strlen(pair) - name_length; // -1 for `=` and +1 for \0
   result = malloc(value_length);
   strncpy(result, pair + name_length + 1, value_length);
   return result;
+}
+
+void env_set(char*** env, char* name, char* value) {
+  int index;
+  char* pair;
+  unsigned int name_length, value_length, length;
+
+  name_length = strlen(name);
+  value_length = strlen(value);
+
+  if ((index = env__find(*env, name)) == -1) {
+    /*
+     * No pair found. Add one. Reallocate memory assigned to env pairs to make
+     * place for new environment variable.
+     */
+    length = env_length(*env);
+    *env = realloc(*env, (length + 2) * sizeof(char*));
+
+    pair = malloc(name_length + 1 + value_length + 1);
+    strcat(pair, name);
+    strcat(pair, "=");
+    strcat(pair, value);
+
+    (*env)[length] = pair;
+    (*env)[length + 1] = NULL;
+  }
+
+  pair = malloc(name_length + 1 + value_length + 1);
+  strcat(pair, name);
+  strcat(pair, "=");
+  strcat(pair, value);
+  (*env)[index] = pair;
 }
