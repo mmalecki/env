7db6957ac1eaceca26a064b789e12ff415f6c9de
Maciej Małecki <me@mmalecki.com>
2013-03-28 02:45:24 +0100

    [refactor] Abstract `env__find`

--- src/env.c
+++ src/env.c
@@ -3,22 +3,33 @@
 
 #include "env.h"
 
-char* env_get(char** env, char* name) {
+char* env__find(char** env, char* name) {
   int i = 0;
   char* pair;
-  char* result;
   unsigned int name_length = strlen(name);
-  unsigned int value_length;
 
   while ((pair = env[i++]) != NULL) {
     if (strncmp(name, pair, name_length) == 0) {
-      value_length = strlen(pair) - name_length; // -1 for `=` and +1 for \0
-      result = malloc(value_length);
-      strncpy(result, pair + name_length + 1, value_length);
-
-      return result;
+      return pair;
     }
   }
 
   return NULL;
+}
+
+char* env_get(char** env, char* name) {
+  char* pair;
+  char* result;
+  unsigned int name_length, value_length;
+
+  name_length = strlen(name);
+  pair = env__find(env, name);
+
+  if (pair == NULL)
+    return NULL;
+
+  value_length = strlen(pair) - name_length; // -1 for `=` and +1 for \0
+  result = malloc(value_length);
+  strncpy(result, pair + name_length + 1, value_length);
+  return result;
 }
